12f63d991439db5d43f57182bd1a562770e207bc
sneha kumari <snehakumari8801@gmail.com>
2026-04-20 19:25:56 +0530

    add baseurl

--- frontend/vite-project/src/api.jsx
+++ frontend/vite-project/src/api.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+//   baseURL: "http://localhost:5000/api"
+baseURL:"https://rolebase-catf.onrender.com/api"
 });
 
 export const setAuthToken = (token) => {
